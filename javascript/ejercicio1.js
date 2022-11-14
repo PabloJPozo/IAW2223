dcc597e41944301bd8f6286852b20003cbd53dc6
pablopozo03 <pablojesuspozo@gmail.com>
2022-11-14 14:11:36 +0100

    update

--- javascript/ejercicio1.js
+++ javascript/ejercicio1.js
@@ -1,12 +1,14 @@
-function ocultarcirculo(id) 
-{
+function ocultar(circulo){
+    document.getElementById(circulo).style.backgroundColor='transparent';
+}
+
+function mostrar(circulo){
+    if (circulo=="roja")
+        document.getElementById(circulo).style.backgroundColor="red";
+    else if(circulo=="azul")
+        document.getElementById(circulo).style.backgroundColor="blue";
+    else if(circulo=="verde")
+        document.getElementById(circulo).style.backgroundColor="green";
     
-    if (document.getElementById(id).style.visibility == "hidden")
-    {
-        document.getElementById(id).style.visibility = "visible";
-    }
-    else{
-        document.getElementById(id).style.visibility = "hidden";
-    }
-   
+     
 }
